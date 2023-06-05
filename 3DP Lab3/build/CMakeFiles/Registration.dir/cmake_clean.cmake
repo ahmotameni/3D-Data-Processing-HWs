dfda1d63f145b39660dacc4411a864fcd2b97331
Amirhossein Motameni <a.h.motameni@gmail.com>
2023-06-05 16:51:14 +0200

    Final working version

---
+++ 3DP Lab3/build/CMakeFiles/Registration.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Registration.dir/src/Registration.cpp.o"
+  "CMakeFiles/Registration.dir/src/Registration.cpp.o.d"
+  "libRegistration.a"
+  "libRegistration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Registration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 3DP_Lab3/build/CMakeFiles/registration.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Registration.
-# This may be replaced when dependencies are built.
